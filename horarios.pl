0301be0c1d4e49b4619fb1d15c84546fc7a94658
Simón Ramírez <sera.-@hotmail.com>
2020-04-12 18:32:04 -0500

    Se agregan reglas para revisar traslapes

--- horarios.pl
+++ horarios.pl
@@ -4,79 +4,105 @@
 
 /*	Hechos	*/
 
-clase(bd,lunes,hoario(7,9)).
-clase(so,lunes,hoario(7,9)).
-clase(redes,lunes,hoario(7,9)).
-clase(ia,lunes,hoario(7,9)).
-clase(sisExp,lunes,hoario(7,9)).
-clase(progGrafica,lunes,hoario(7,9)).
-clase(seguridad,lunes,hoario(7,9)).
+clase(bd,lunes,7,9).
+clase(so,lunes,7,9).
+clase(redes,lunes,7,9).
+clase(ia,lunes,7,9).
+clase(sisExp,lunes,7,9).
+clase(progGrafica,lunes,7,9).
+clase(seguridad,lunes,7,9).
 
-clase(bd,lunes,hoario(8,10)).
-clase(so,lunes,hoario(8,10)).
-clase(redes,lunes,hoario(8,10)).
-clase(ia,lunes,hoario(8,10)).
-clase(sisExp,lunes,hoario(8,10)).
-clase(progGrafica,lunes,hoario(8,10)).
-clase(seguridad,lunes,hoario(8,10)).
+clase(bd,lunes,8,10).
+clase(so,lunes,8,10).
+clase(redes,lunes,8,10).
+clase(ia,lunes,8,10).
+clase(sisExp,lunes,8,10).
+clase(progGrafica,lunes,8,10).
+clase(seguridad,lunes,8,10).
 
-clase(bd,lunes,hoario(9,11)).
-clase(so,lunes,hoario(9,11)).
-clase(redes,lunes,hoario(9,11)).
-clase(ia,lunes,hoario(9,11)).
-clase(sisExp,lunes,hoario(9,11)).
-clase(progGrafica,lunes,hoario(9,11)).
-clase(seguridad,lunes,hoario(9,11)).
+clase(bd,lunes,9,11).
+clase(so,lunes,9,11).
+clase(redes,lunes,9,11).
+clase(ia,lunes,9,11).
+clase(sisExp,lunes,9,11).
+clase(progGrafica,lunes,9,11).
+clase(seguridad,lunes,9,11).
 
-clase(bd,lunes,hoario(10,12)).
-clase(so,lunes,hoario(10,12)).
-clase(redes,lunes,hoario(10,12)).
-clase(ia,lunes,hoario(10,12)).
-clase(sisExp,lunes,hoario(10,12)).
-clase(progGrafica,lunes,hoario(10,12)).
-clase(seguridad,lunes,hoario(10,12)).
+clase(bd,lunes,10,12).
+clase(so,lunes,10,12).
+clase(redes,lunes,10,12).
+clase(ia,lunes,10,12).
+clase(sisExp,lunes,10,12).
+clase(progGrafica,lunes,10,12).
+clase(seguridad,lunes,10,12).
 
-clase(bd,martes,hoario(7,9)).
-clase(so,martes,hoario(7,9)).
-clase(redes,martes,hoario(7,9)).
-clase(ia,martes,hoario(7,9)).
-clase(sisExp,martes,hoario(7,9)).
-clase(progGrafica,martes,hoario(7,9)).
-clase(seguridad,martes,hoario(7,9)).
+clase(bd,martes,7,9).
+clase(so,martes,7,9).
+clase(redes,martes,7,9).
+clase(ia,martes,7,9).
+clase(sisExp,martes,7,9).
+clase(progGrafica,martes,7,9).
+clase(seguridad,martes,7,9).
 
-clase(bd,martes,hoario(8,10)).
-clase(so,martes,hoario(8,10)).
-clase(redes,martes,hoario(8,10)).
-clase(ia,martes,hoario(8,10)).
-clase(sisExp,martes,hoario(8,10)).
-clase(progGrafica,martes,hoario(8,10)).
-clase(seguridad,martes,hoario(8,10)).
+clase(bd,martes,8,10).
+clase(so,martes,8,10).
+clase(redes,martes,8,10).
+clase(ia,martes,8,10).
+clase(sisExp,martes,8,10).
+clase(progGrafica,martes,8,10).
+clase(seguridad,martes,8,10).
 
-clase(bd,martes,hoario(9,11)).
-clase(so,martes,hoario(9,11)).
-clase(redes,martes,hoario(9,11)).
-clase(ia,martes,hoario(9,11)).
-clase(sisExp,martes,hoario(9,11)).
-clase(progGrafica,martes,hoario(9,11)).
-clase(seguridad,martes,hoario(9,11)).
+clase(bd,martes,9,11).
+clase(so,martes,9,11).
+clase(redes,martes,9,11).
+clase(ia,martes,9,11).
+clase(sisExp,martes,9,11).
+clase(progGrafica,martes,9,11).
+clase(seguridad,martes,9,11).
 
-clase(bd,martes,hoario(10,12)).
-clase(so,martes,hoario(10,12)).
-clase(redes,martes,hoario(10,12)).
-clase(ia,martes,hoario(10,12)).
-clase(sisExp,martes,hoario(10,12)).
-clase(progGrafica,martes,hoario(10,12)).
-clase(seguridad,martes,hoario(10,12)).
+clase(bd,martes,10,12).
+clase(so,martes,10,12).
+clase(redes,martes,10,12).
+clase(ia,martes,10,12).
+clase(sisExp,martes,10,12).
+clase(progGrafica,martes,10,12).
+clase(seguridad,martes,10,12).
 
 /*	clase(materia, dia, horaInicio, horaFin)	*/
 
 /*	Reglas */
-mayorIgual(horario(HoraA, _), horario(_, HoraB)):-
-	HoraA >= HoraB.
 
-inscribeMateria(Materia):-
-    clase(Materia, Dia, Horario).
+traslape(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2)):-
+                            clase(Materia1, Dia1, _, Fin1),
+                            clase(Materia2, Dia2, Inicio2, _),
+                            Materia1 \== Materia2,
+                            Inicio2 > Fin1,
+                            Dia1 == Dia2.
 
+traslapeDistintoDia(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2)):- 
+                            clase(Materia1, Dia1, Inicio1, Fin1),
+                            clase(Materia2, Dia2, Inicio2, Fin2),
+                            Materia1 \== Materia2,
+                            Inicio1 == Inicio2,
+                            Dia1 \== Dia2.
+
+inscribeMismoDia(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2), clase(Materia3,Dia3,Inicio3,Fin3)):-
+                            traslape(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2)),
+                            traslape(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia3,Dia3,Inicio3,Fin3)),
+                            traslape(clase(Materia2,Dia2,Inicio2,Fin2), clase(Materia3,Dia3,Inicio3,Fin3)).
+
+inscribeDistintoDia(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2), clase(Materia3,Dia3,Inicio3,Fin3)):-
+                            traslapeDistintoDia(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2)),
+                            traslapeDistintoDia(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia3,Dia3,Inicio3,Fin3)),
+                            traslapeDistintoDia(clase(Materia2,Dia2,Inicio2,Fin2), clase(Materia3,Dia3,Inicio3,Fin3)).
+
+inscribe(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2), clase(Materia3,Dia3,Inicio3,Fin3)):-
+                            inscribeMismoDia(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2), clase(Materia3,Dia3,Inicio3,Fin3));
+                            inscribeDistintoDia(clase(Materia1,Dia1,Inicio1,Fin1), clase(Materia2,Dia2,Inicio2,Fin2), clase(Materia3,Dia3,Inicio3,Fin3)).
+
+
+
+                            
 
 
 
