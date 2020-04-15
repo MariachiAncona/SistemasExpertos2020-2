66a5f2d00e1bdb93bb583bab7e90fed18402018c
Simón Ramírez <sera.-@hotmail.com>
2020-04-15 00:17:43 -0500

    Agrego programa frames.pl modificado y horarios modificado para solo 2 clases.

---
+++ frames.pl
@@ -0,0 +1,50 @@
+/*	HECHOS	*/
+
+esUn(animal, serVivo).
+esUn(vegetal, serVivo).
+esUn(vertebrado, animal).
+esUn(invertebrado, animal).
+esUn(mamifero, vertebrado).
+esUn( carnivoro, mamifero).
+esUn(perro, carnivoro).
+esUn(spike, perro).
+/************************/
+esUn(objeto, materia).
+esUn(mueble, objeto).
+esUn(silla, mueble).
+
+atrib(serVivo, respira).
+atrib(animal, seMueve).
+atrib(gato, garras).
+atrib(vertebrado, colVertebral).
+atrib(mamifero, amamantado).
+atrib(carnivoro, comeCarne).
+atrib(perro, ladra).
+atrib(spike, colorCafe).
+/***********************/
+
+atrib(objeto, masa).
+atrib(mueble, dimensiones).
+atrib(silla, patas).
+
+/*	REGLAS	*/
+
+
+isA(A,B):-
+	esUn(A,B).
+isA(A,B):- 
+	esUn(A,X),
+	isA(X,B).
+
+tieneAtrib(Atrib, Clase):- 
+	atrib(Clase, Atrib),!.
+tieneAtrib(Atrib, Clase):- 
+	isA(Clase, ClaseX),
+	tieneAtrib(Atrib, ClaseX).
+
+
+
+
+   
+
+
